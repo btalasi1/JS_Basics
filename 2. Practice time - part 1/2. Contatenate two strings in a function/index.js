e89ddda09fa61520af9fabf404f8fbb110ff336d
btalasi1 <btalasi1@asu.edu>
2023-02-02 16:07:22 -0700

    Practice Done

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -1,5 +1,11 @@
 let name = "Linda"
 let greeting = "Hi there"
 
+
 // Create a function that logs out "Hi there, Linda!" when called
 
+
+function greeting_user(){
+    console.log(greeting+", "+name+"!")
+}
+greeting_user()
